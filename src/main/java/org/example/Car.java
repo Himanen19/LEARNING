23affd812a5090579527bb98dabb93763881dc6e
Himanen19 <himanentv@gmail.com>
2026-04-09 11:53:09 -0300

    Polymorphisms challange Cars && methods overriding.

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,6 +2,90 @@ package org.example;
 
 public class Car {
 
+    private String description;
+
+    public Car(String description) {
+        this.description = description;
+    }
+
+    public void startEngine() {
+
+        System.out.println("the car is started" + description);
+        this.runEngine();
+
+    }
+
+    public void drive() {
+
+        System.out.println("You are driving on the highwaysss! with a : " + description + " and that's a: " + getClass().getSimpleName());
+        runEngine();
+
+    }
+
+    protected void runEngine() {
+
+        System.out.println("the engine is on fire!!!!");
+
+    }
+
+}
+
+
+class GasPoweredCar extends Car {
+
+    private double avgKmPerLitre;
+    private int cylinders = 6;
+
+    public GasPoweredCar(String description, double avgKmPerLitre, int cylinders) {
+        super(description);
+        this.cylinders = cylinders;
+        this.avgKmPerLitre = avgKmPerLitre;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("this is a GAS car and have %d Cylinders that are ready \n", cylinders);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("this is a GAS car and makes %f KM per Litre on avg \n", avgKmPerLitre);
+    }
+}
+
+class EletricCar extends Car {
+    private double avgKmPerCharge;
+    private int batterySize;
+    public EletricCar(String description) {
+        super(description);
+    }
+
+
+    public EletricCar(String description, int batterySize, double avgKmPerCharge) {
+        super(description);
+        this.batterySize = batterySize;
+        this.avgKmPerCharge = avgKmPerCharge;
+    }
+
+    public void topSpeed() {
+
+        System.out.println("A eletric cars top speed is 200km/h and it makes a avg of " + avgKmPerCharge + " and have up to " + batterySize + " battery size.");
+    }
+}
+
+class HybridCar extends Car {
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders;
+    public HybridCar(String description) {
+        super(description);
+    }
+
+}
+
+
+class Car1 {
+
     private String make;
     private String model;
     private String color;
