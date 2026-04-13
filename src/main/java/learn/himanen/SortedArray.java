a025f1964eaead41ec5ef3adacca72ede06a8e1a
Himanen19 <himanentv@gmail.com>
2026-04-13 17:20:54 -0300

    learning

--- src/main/java/learn/himanen/SortedArray.java
+++ src/main/java/learn/himanen/SortedArray.java
@@ -1,6 +1,8 @@
 package learn.himanen;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class SortedArray {
@@ -36,4 +38,59 @@ public class SortedArray {
         return desArr;
     }
 
+    private static int readInteger() {
+
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        return value;
+    }
+
+    private static int[] readElements(int ele) {
+        int[] finalResult = new int[ele];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < ele; i++) {
+            finalResult[i] = scanner.nextInt();
+        }
+
+
+        return finalResult;
+    }
+
+    private static int findMin(int[] arr) {
+
+        Arrays.sort(arr);
+        int minValue = arr[0];
+
+        Integer[] b = {1, 2, 3, 4};
+        ArrayList<Integer> a = new ArrayList<>(List.of(b));
+
+        Test test = new Test();
+        return minValue;
+
+
+    }
+
+    private static void reverse(int[] array) {
+
+        int[] reversedArray = new int[array.length];
+        System.out.printf("Array = %s", Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[array.length - 1 - i] = array[i];
+
+
+        }
+        System.out.printf("Reversed array = %s", Arrays.toString(reversedArray));
+
+
+    }
+
+    record Test(String name, int num, boolean t) {
+
+        Test() {
+            this("jao", 3, true);
+        }
+    }
+
+
 }
+
