94d4398e5a913ba853e82ea8cf6bf4a5526e8002
Himanen19 <himanentv@gmail.com>
2026-04-14 09:02:54 -0300

    Creating ArrayLists With User Inputs

--- src/main/java/learn/himanen/CreateArrayListWithUserInput.java
+++ src/main/java/learn/himanen/CreateArrayListWithUserInput.java
@@ -7,8 +7,9 @@ import java.util.Scanner;
 public class CreateArrayListWithUserInput {
 
 
+    private static Scanner scanner = new Scanner(System.in);
     public static void askUser() {
-        Scanner scanner = new Scanner(System.in);
+
         ArrayList<String> theList = new ArrayList<>();
         boolean flag = true;
         while (flag) {
@@ -17,35 +18,63 @@ public class CreateArrayListWithUserInput {
             System.out.println("0 to exit\n 1 to add\n 2 to remove\n");
             try {
                 int userCommand = scanner.nextInt();
-                if (userCommand == 0) {
-                    System.out.println("System is shuting down");
-                    flag = false;
-
-                } else if (userCommand == 1) {
-                    scanner.nextLine();
-                    System.out.println("Type the itens to add");
-                    userInput = scanner.nextLine();
-                    if (theList.contains(userInput)) {
-                        System.out.println("Duplicated itens not allowed");
-                    } else {
-                        theList.add(userInput);
-                        theList.sort(Comparator.naturalOrder());
-                        System.out.println(theList.toString());
+                switch (userCommand) {
+                    case 0 -> {
+                        System.out.println("System is shuting down");
+                        flag = false;
                     }
-                } else if (userCommand == 2) {
-                    System.out.println("Type the itens to remove");
-                    scanner.nextLine();
-                    userInput = scanner.nextLine();
-                    theList.remove(userInput);
-                    theList.sort(Comparator.naturalOrder());
-                    System.out.println(theList.toString());
-                } else {
-                    System.out.println("Command not allowed!!!!!");
+                    case 1 -> addItems(theList);
+                    case 2 -> removeItems(theList);
+                    default -> System.out.println("Command not allowed!!!!!");
                 }
+//                if (userCommand == 0) {
+//                    System.out.println("System is shuting down");
+//                    flag = false;
+//
+//                } else if (userCommand == 1) {
+//                 addItems(theList);
+//
+//                } else if (userCommand == 2) {
+//                  removeItems(theList);
+//                } else {
+//                    System.out.println("Command not allowed!!!!!");
+//                }
             } catch (Exception e) {
                 System.out.println("Type a fucking number mate");
                 break;
             }
         }
+    }
+
+    private static void addItems(ArrayList<String> groceries) {
+        scanner.nextLine();
+        System.out.println("Add itens separeted by ,");
+        String[] items = scanner.nextLine().split(",");
+        for (String item : items) {
+            item = item.trim();
+            if (groceries.contains(item)) {
+                System.out.println("Duplicated itens not allowed");
+            } else {
+                groceries.add(item);
+            }
+        }
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries.toString());
+
+
+    }
+
+    private static void removeItems(ArrayList<String> groceries) {
+        scanner.nextLine();
+        System.out.println("Remove itens separeted by ,");
+        String[] items = scanner.nextLine().split(",");
+        for (String item : items) {
+            item = item.trim();
+            groceries.remove(item);
+        }
+        groceries.sort(Comparator.naturalOrder());
+        System.out.println(groceries.toString());
+
+
     }
 }
